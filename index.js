6942eeb33969435ca53694c80ebf8cecde1e0698
ayushijain97 <neeraj_jain@apple.com>
2021-09-10 13:56:34 +0530

    api integration for login

--- index.js
+++ index.js
@@ -1,7 +1,77 @@
-$(function() {
-    $("#login__btn").click(function() {
-        let mobileNumber = $("#mobileNumber").val();
-        localStorage.setItem("loggedInUser", mobileNumber);
-        location.href = "/items.html";
+$(() => {
+    $("#continueBtn").click(function() {
+        let mobileNumber = $("#mobile_Number").val();
+        if (mobileNumber.length === 10) {
+            changingStyles();
+            gettingOTP(mobileNumber);
+            $("#login__btn").click(loggingIn);
+        } else {
+            enableToaster();
+        }
     })
 });
+const gettingOTP = async(mobileNumber) => {
+
+    try {
+        const result = await fetch("https://niobooks.in/api/web/request_otp", {
+            method: "POST",
+            headers: {
+                'Accept': 'application/JSON',
+                'Content-type': 'application/JSON',
+                'client': 'web'
+            },
+            body: JSON.stringify({
+                'mobile_number': mobileNumber,
+            })
+        });
+
+        const data = await result.json();
+        console.log(data);
+    } catch (err) {
+        alert(err);
+    }
+}
+const changingStyles = () => {
+    $("#continueBtn").css({ display: "none" });
+    $("#inputOTP").css({ display: "block" });
+    $("#login__btn").css({ display: "block" });
+}
+
+const loggingIn = async() => {
+    let otp = $("#otpInput").val();
+    let mobileNumber = $("#mobile_Number").val();
+    if (otp.length === 6) {
+        try {
+            const result = await fetch("https://niobooks.in/api/web/authenticate", {
+                method: "POST",
+                headers: {
+                    'Accept': 'application/JSON',
+                    'Content-type': 'application/JSON',
+                    'client': 'web'
+                },
+                body: JSON.stringify({
+                    mobile_number: mobileNumber,
+                    otp_code: otp
+                })
+            });
+            const userDetails = await result.json();
+            // enter you logic when the fetch is successful
+            localStorage.setItem("loggedInUser", JSON.stringify(userDetails));
+            location.href = "/items.html";
+        } catch (err) {
+            alert(err);
+        }
+
+    } else {
+        $("#Toast").toast('show');
+        setTimeout(function() {
+            $("#Toast").toast('hide');
+        }, 5000)
+    }
+}
+const enableToaster = () => {
+    $("#myToast").toast('show');
+    setTimeout(function() {
+        $("#myToast").toast('hide');
+    }, 5000)
+}
--- items.js
+++ items.js
@@ -1,78 +1,116 @@
- let $table = $('#table')
+ let $table = $('#table');
+ let $itemName = $("#item-name");
+ let $itemCode = $("#item-code");
+ let $itemUnit = $("#item-unit");
+ let $itemDate = $("#item-date");
+ let $itemSalesPrice = $("#item-sales-price");
+ let $itemPurchasePrice = $("#item-purchase-price");
  let currentUserKey = "";
+ let existingItems;
+ let itemsForm;
 
  $(function() {
      console.log("ready!");
      // 1. Checking if user is loggedIn
      const loggedInUser = getLoggedInUser();
-     $("#loggedInUser").text(loggedInUser);
+     const number = loggedInUser.mobile_number;
+     console.log(number);
+     $("#loggedInUser").text(number);
 
      // 2. Creating unique key For User
-     currentUserKey = `items-${loggedInUser}`;
+     currentUserKey = `items-${number}`;
 
      // 3. Checking if existing items are present
      let existingItems = JSON.parse(localStorage.getItem(currentUserKey));
+
 
      // 4. initializing the  items in the table
      var data = existingItems;
      $table.bootstrapTable({
          data: data ? data : [],
-         onClickRow: function(arg1, arg2) {
+         onClickRow: function(arg1) {
              console.log(arg1);
-             console.log(arg2);
+             editItemsRow(arg1);
          }
-     })
+     });
+
+
+     // Fetch all the forms we want to apply custom Bootstrap validation styles to
+     var forms = document.getElementsByClassName('needs-validation');
+     // Loop over them and prevent submission
+     var validation = Array.prototype.filter.call(forms, function(form) {
+         itemsForm = form;
+         form.addEventListener('submit', function(event) {
+             if (form.checkValidity() === false) {
+                 event.preventDefault();
+                 event.stopPropagation();
+                 form.classList.add('was-validated');
+             } else {
+                 onSubmit();
+             }
+         }, false);
+     });
+
  });
 
- $("#item-save-button").click(function() {
-     const item = {
-         "name": $("#item-name").val(),
-         "code": $("#item-code").val(),
-         "unit": $("#item-unit").val(),
-         "date": $("#item-date").val(),
-         "salesPrice": $("#item-sales-price").val(),
-         "purchasePrice": $("#item-purchase-price").val()
+ const onSubmit = () => {
+     let item = {
+         "name": $itemName.val(),
+         "code": $itemCode.val(),
+         "unit": $itemUnit.val(),
+         "date": $itemDate.val(),
+         "salesPrice": $itemSalesPrice.val(),
+         "purchasePrice": $itemPurchasePrice.val()
      }
 
-     // Create uniqueId for this new Item.
-     // 1. Check globalId counter from localStorage
-     // 2. if exist increment and reuse
-     // 3. Store the incremented value in localStorage
-     const itemID = parseInt(localStorage.getItem("itemId"));
-     localStorage.setItem("itemId", !itemID ? 1 : itemID + 1);
-     item["id"] = localStorage.getItem("itemId");
 
-     //  Pushing item to localStorage 
-     let existingItems = JSON.parse(localStorage.getItem(currentUserKey));
-     if (!existingItems) {
-         existingItems = [];
+     // Check if update or create flow
+     const itemID = $("#item-id").val();
+
+     if (itemID) {
+         // Update Item flow
+         updateItem(item, itemID);
+     } else {
+         // Create newItem flow
+         createNewItem(item);
      }
-     existingItems.push(item);
-     localStorage.setItem(currentUserKey, JSON.stringify(existingItems));
-
-     // Let's append this new record to the table
-     $table.bootstrapTable('append', item);
 
      // Also clear the form
-     clearForm();
- });
+     //  clearForm();
+     itemsForm.reset();
+     itemsForm.classList.remove('was-validated');
+ }
+
+ const validForm = () => {
+
+     if ($itemName.val() === "" || $itemCode.val() === "" || $itemUnit.val() === "" ||
+         $itemDate.val() === "" || $itemSalesPrice.val() === "" || $itemPurchasePrice.val() === "") {
+         $("#myToast").toast('show');
+         setTimeout(function() {
+             $("#myToast").toast('hide');
+         }, 5000)
+         return false;
+     }
+     return true;
+ }
+
  //loggingOut clicking on Logout
  $("#logout-button").click(function() {
      logOut();
  });
 
- const editItemRow = () => {
-     const counter = 1;
-     if (!counter) {
-         localStorage.setItem("itemId", counter);
-     } else {
-         localStorage.setItem("itemId", counter++);
-     }
-
+ const editItemsRow = (rowData) => {
+     $itemName.val(rowData.name);
+     $itemCode.val(rowData.code);
+     $itemUnit.val(rowData.unit);
+     $itemDate.val(rowData.date);
+     $itemSalesPrice.val(rowData.salesPrice);
+     $itemPurchasePrice.val(rowData.purchasePrice);
+     $("#item-id").val(rowData.id);
  }
 
  const getLoggedInUser = () => {
-     const loggedInUser = localStorage.getItem("loggedInUser");
+     const loggedInUser = JSON.parse(localStorage.getItem("loggedInUser"));
      if (!loggedInUser) {
          location.href = "/index.html";
      }
@@ -93,6 +131,49 @@
      $("#item-purchase-price").val("");
  }
 
- const clickMe = function() {
-     alert("Hello");
+ const updateItem = (item, itemID) => {
+
+     const items = JSON.parse(localStorage.getItem(currentUserKey));
+     let index;
+     for (index = 0; index < items.length; index++) {
+         if (items[index].id === itemID) {
+             break;
+         }
+     }
+
+     // Now we have the item Row in table
+     $table.bootstrapTable('updateRow', {
+         index: index,
+         row: item
+     });
+
+     // Now update localStorage as well
+     item["id"] = itemID;
+     items[index] = item;
+
+     localStorage.removeItem(currentUserKey);
+     localStorage.setItem(currentUserKey, JSON.stringify(items));
+
+ }
+
+ const createNewItem = (item) => {
+     // Create uniqueId for this new Item.
+     // 1. Check globalId counter from localStorage
+     // 2. if exist increment and reuse
+     // 3. Store the incremented value in localStorage
+     const itemID = parseInt(localStorage.getItem("itemId"));
+
+
+     localStorage.setItem("itemId", !itemID ? 1 : itemID + 1);
+     item["id"] = localStorage.getItem("itemId");
+     //  Pushing item to localStorage 
+     let existingItems = JSON.parse(localStorage.getItem(currentUserKey));
+     if (!existingItems) {
+         existingItems = [];
+     }
+     existingItems.push(item);
+     localStorage.setItem(currentUserKey, JSON.stringify(existingItems));
+
+     // Let's append this new record to the table
+     $table.bootstrapTable('append', item);
  }
